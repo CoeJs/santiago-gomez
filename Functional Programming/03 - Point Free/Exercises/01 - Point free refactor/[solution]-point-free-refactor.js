33d6e3d54ed83f5007cab9a24297c3dfb6626c01
Santiago Gómez Grajales <santiago.gomez@unosquare.com>
2024-01-02 14:33:37 -0500

    Updated FP docs

---
+++ Functional Programming/03 - Point Free/Exercises/01 - Point free refactor/[solution]-point-free-refactor.js
@@ -0,0 +1,33 @@
+"use strict";
+
+function not(fn) {
+    return function negated(...args) {
+        return !fn(...args);
+    }
+}
+
+function when(fn) {
+	return function(predicate) {
+		return function(...args) {
+			if (predicate(...args)) {
+				return fn(...args);
+			}
+		};
+	};
+}
+
+function isShortEnough(str) {
+	return str.length <= 5;
+}
+
+isLongEnough = not(isShortEnough);
+output = console.log.bind(console);
+printIf = when(output);
+
+var msg1 = "Hello";
+var msg2 = msg1 + " World";
+
+printIf(isShortEnough)(msg1);		// Hello
+printIf(isShortEnough)(msg2);
+printIf(isLongEnough)(msg1);
+printIf(isLongEnough)(msg2);		// Hello World
